db750c64c48fe078ff132b871daa39d01345972e
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-10-05 17:13:11 +0900

    ✨ feat: add static part of the application
    - add AccordionItem component with props
    - use i variable that means index number in the array.map()

--- src/App.js
+++ src/App.js
@@ -18,11 +18,28 @@ const faqs = [
 export default function App() {
   return (
     <div>
-      <Accordion />
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-function Accordion() {
-  return <div>TODO</div>;
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem title={el.title} text={el.text} num={i} />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  return (
+    <div className="item">
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon"></p>
+      <div className="content-box">{text}</div>
+    </div>
+  );
 }
